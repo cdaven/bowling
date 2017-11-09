20cb73689af8420b34974bf8db6ede35094b33dc
Christian Davén <christian@daven.se>
2017-11-09 20:57:31 +0100

    Added Roll.MAX_PINS

--- Bowling/Roll.cs
+++ Bowling/Roll.cs
@@ -8,11 +8,13 @@ namespace Bowling
 {
     class Roll
     {
+        const int MAX_PINS = 10;
+        
         public int Pins { get; set; } = 0;
         public int Bonus { get; private set; } = 0;
         public int Score => Pins + Bonus;
 
-        public bool Strike => Pins == 10;
+        public bool Strike => Pins == MAX_PINS;
         public bool Spare { get; set; } = false;
 
         public void AddBonus(int pins)
