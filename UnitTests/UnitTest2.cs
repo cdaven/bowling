ff758ec730f0c7539874721efe613cb786851a97
Christian Davén <christian@daven.se>
2017-11-10 10:32:26 +0100

    Added unit tests for 7-frame games

---
+++ UnitTests/UnitTest2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bowling;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestSevenFramesZeroes()
+        {
+            var game = new Game(numFrames: 7);
+            game.Roll(0); game.Roll(0);
+            game.Roll(0); game.Roll(0);
+            game.Roll(0); game.Roll(0);
+            game.Roll(0); game.Roll(0);
+            game.Roll(0); game.Roll(0);
+            game.Roll(0); game.Roll(0);
+            game.Roll(0); game.Roll(0);
+            Assert.AreEqual(0, game.Score());
+            Assert.IsTrue(game.Completed);
+        }
+
+        [TestMethod]
+        public void TestSevenFramesStrikes()
+        {
+            var game = new Game(numFrames: 7);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10); 
+            Assert.AreEqual(7 * 30, game.Score());
+            Assert.IsTrue(game.Completed);
+        }
+    }
+}
